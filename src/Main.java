79f48ae1b8952c78df7631b67d21cdd2e0582f54
ALEKSEY BUDAEV <irbis7fox@list.ru>
2022-09-21 20:18:50 +0300

    jjj

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int score = 350;
+        int amount =1745;
+        int bonus;
+        int x = 100;
+        if (amount > 1000) {
+            System.out.println(bonus = amount / x);
+            System.out.println(score + amount + bonus);
+        } else {
+            System.out.println(score + amount);
+        }
+    }
+}
